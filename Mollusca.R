8946e2795b58a87bb7c433ca0843ee3e8a629bad
Christian Bermúdez-Rivas <41581329+ChrisBermudezR@users.noreply.github.com>
2023-07-26 18:10:52 -0500

    Adición de scripst, datos y biblio

---
+++ Mollusca.R
@@ -0,0 +1,118 @@
+if(!require("spocc"))install.packages("spocc")
+if(!require("tidyverse"))install.packages("tidyverse")
+if(!require("robis"))install.packages("robis")
+
+spp= c("Firoloida desmarestia")
+F_desmarestia = spocc::occ(query = c('Firoloida desmarestia', 
+                                     'Cyclostrema minutum', 
+                                     'Cyclostrema solutum',
+                                     'Firola gaimardi',
+                                     'Firola lesueurii',
+                                     'Firola liguriae',
+                                     'Firolella gracilis',
+                                     'Firolella vigilans',
+                                     'Firoloida aculeata',
+                                     'Firoloida blainvilleana',
+                                     'Firoloida demarestia',
+                                     'Firoloida kowalewskyi',
+                                     'Firoloida liguriae',
+                                     'Pterotrachea lesueri'), 
+                            from = c('gbif', 
+                                     'inat',
+                                     'bison',
+                                     'obis',
+                                     'idigbio'), 
+limit = 50000)
+head(F_desmarestia)
+
+Occurrencias_F_desmarestia=spocc::occ2df(F_desmarestia)
+
+Occurrencias_F_desmarestia_df<-as.data.frame(Occurrencias_F_desmarestia)
+
+Occurrencias_F_desmarestia_df$name<-as.factor(Occurrencias_F_desmarestia_df$name)
+
+write.table(Occurrencias_F_desmarestia_df, "./OccData/F_desmarestia.csv",dec = ".", sep = ",", col.names = TRUE, row.names = FALSE, na="NA")
+
+#####
+
+OccObis<-robis::occurrence(scientificname = c('Firoloida desmarestia',
+                                              'Cyclostrema minutum',
+                                              'Cyclostrema solutum',
+                                              'Firola gaimardi',
+                                              'Firola lesueurii',
+                                              'Firola liguriae',
+                                              'Firolella gracilis',
+                                              'Firolella vigilans',
+                                              'Firoloida aculeata',
+                                              'Firoloida blainvilleana',
+                                              'Firoloida demarestia',
+                                              'Firoloida kowalewskyi',
+                                              'Firoloida liguriae',
+                                              'Pterotrachea lesueri'),
+                           fields=c("year", "month", "day"))
+
+selectObis<-as.data.frame(cbind(OccObis$scientificName,OccObis$decimalLongitude,OccObis$decimalLatitude,OccObis$prov,OccObis$eventDate,OccObis$id))
+prov<-rep('Obis',82)
+selectObis$prov<-prov
+colnames(selectObis)<-c('name','longitude','latitude','date','key','prov')
+selectObis<-selectObis[c('name','longitude','latitude','prov','date','key')]
+rbind(Occurrencias_F_desmarestia_df,selectObis)
+colnames(selectObis)
+colnames(Occurrencias_F_desmarestia_df)
+
+##Extraer columnas del Occurrence de Darwin Core
+OccObis<-robis::occurrence(scientificname = c('Firoloida desmarestia',
+                                              'Cyclostrema minutum',
+                                              'Cyclostrema solutum',
+                                              'Firola gaimardi',
+                                              'Firola lesueurii',
+                                              'Firola liguriae',
+                                              'Firolella gracilis',
+                                              'Firolella vigilans',
+                                              'Firoloida aculeata',
+                                              'Firoloida blainvilleana',
+                                              'Firoloida demarestia',
+                                              'Firoloida kowalewskyi',
+                                              'Firoloida liguriae',
+                                              'Pterotrachea lesueri'),
+                           fields=c("scientificName","decimalLongitude","decimalLatitude","eventDate","year","month","day","id"))
+
+write.table(OccObis, "OccObis.csv", na="NA", row.names = FALSE, col.names = TRUE, sep = ",")
+
+OccObis<-read.table("OccObis.csv", sep = ",", dec = ".", header = TRUE)
+
+class(OccObis$year)
+
+
+##Convertir números a fecha y concatenar las columnas 
+
+
+
+
+#OccObis$date2<-as.Date((OccObis$date), "%y-%m-%d")
+OccObis$year<-  as.numeric(format(as.Date(OccObis$year), "%Y"))
+OccObis$month<-as.numeric(format(as.Date(OccObis$month), "%m"))
+OccObis$day<-as.numeric(format(as.Date(OccObis$day), "%d"))
+
+OccObis$Fecha<-  with(OccObis, paste(format(as.Date(OccObis$year), "%Y"), 
+                      format(as.Date(OccObis$month), "%m"), 
+                      format(as.Date(OccObis$day), "%d"),
+                      sep="-"))
+
+OccObis$Fecha[OccObis$Fecha == 'NA-NA-NA']<-NA
+OccObis$Fecha<-as.Date(OccObis$Fecha)
+OccObis$prov<-"obis"
+colnames(OccObis)<-c("latitude", "longitude", "eventDate", "name", "key", "day", "month", "year", "date", "prov")
+obisT<-as.data.frame(cbind(OccObis$name, OccObis$longitude, OccObis$latitude, OccObis$prov, format(as.Date(OccObis$date), "%Y-%m-%d") , OccObis$key))
+colnames(obisT)<-colnames(Occurrencias_F_desmarestia)
+  
+Occurrencias_F_desmarestia$date<-as.character(Occurrencias_F_desmarestia$date)
+
+
+total<-rbind(obisT, Occurrencias_F_desmarestia)
+total$date<-as.Date(total$date)
+class(total$date)
+
+duplicated(total)
+
+write.table(total, "Fdesmarestia_Occ_totales.csv", col.names = TRUE, row.names = FALSE, sep=",", dec=".")
